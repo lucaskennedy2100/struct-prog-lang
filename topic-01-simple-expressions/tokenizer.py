1d0136118b075b90703501a9fcc96efa33794f82
Lucas <lucaskennedy2100@gmail.com>
2026-01-21 17:02:29 -0500

    first coding lecture(tokenizerPt1

--- topic-01-simple-expressions/tokenizer.py
+++ topic-01-simple-expressions/tokenizer.py
@@ -1,8 +1,34 @@
 import re
 
-p = re.compile("ab*")
+#p = re.compile("ab*")
 
-if p.match("abbbbbbb") :
-    print("match")
-else:
-    print("not match")
+#if p.match("abbbbbbb") :
+ #   print("match")
+#else:
+ #   print("not match")
+ #declare a list of possible regex operators
+patterns = [
+    (r"\s+", "whitespace"),
+    (r"\d+", "number"),
+    (r"\+", "+"),
+    (r"\-", "-"),
+    (r"/", "/"),
+    (r"\*", "*"),
+    (r".", "error") #only use everything if nothing else matches
+]
+
+patterns = [re.compile(p), tag) for p, tag in patterns]
+
+def tokenize(characters):
+    "Tokenize a string using the patterns above"
+    tokens = []
+    position = 0
+    line = 1
+    column = 1
+
+    while position < len(characters):
+        for pattern, tag in patterns:
+            match = pattern.match(characters, position)
+            if match:
+                break
+        assert match is not None
