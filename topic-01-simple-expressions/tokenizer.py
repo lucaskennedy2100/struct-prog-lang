a538f365a5a7934fbb81d51c0dcb86d65523e74a
Lucas <lucaskennedy2100@gmail.com>
2026-01-28 16:40:02 -0500

    lecture 4--finish tokenizer

--- topic-01-simple-expressions/tokenizer.py
+++ topic-01-simple-expressions/tokenizer.py
@@ -1,4 +1,5 @@
 import re
+from pprint import pprint #as print #this prints left aligned. if you do it with  as print on the end pprint() becomes a fuinction to do this
 
 #p = re.compile("ab*")
 
@@ -12,12 +13,12 @@ patterns = [
     (r"\d+", "number"),
     (r"\+", "+"),
     (r"\-", "-"),
-    (r"/", "/"),
+    (r"\/", "/"),
     (r"\*", "*"),
     (r".", "error") #only use everything if nothing else matches
 ]
 
-patterns = [re.compile(p), tag) for p, tag in patterns]
+patterns = [(re.compile(p), tag) for p, tag in patterns] #Patterns is assigned new values--turns from string and tag to compiled pattern and tag
 
 def tokenize(characters):
     "Tokenize a string using the patterns above"
@@ -25,10 +26,86 @@ def tokenize(characters):
     position = 0
     line = 1
     column = 1
+    current_tag = None
 
-    while position < len(characters):
+    while position < len(characters): #while we've not run out of characters
         for pattern, tag in patterns:
             match = pattern.match(characters, position)
             if match:
-                break
+                current_tag = tag
+                break #if we encounter a regex, break
         assert match is not None
+        value = match.group(0)
+
+        if current_tag == "error":
+            raise Exception(f"Unexpected character: {value!r}")
+        
+        if tag != "whitespace":
+            token = {"tag": current_tag, "line": line, "column": column}
+            if current_tag == "number":
+                token["value"] = int(value)
+            tokens.append(token) #if we don't have whitespace, append the token to the end of our tokens list
+        
+        #now advance the position in the line
+        for ch in value:
+            if ch =="\n":
+                line += 1
+                column += 1
+            else:
+                column +=1
+        position = match.end()
+
+    #now add a ending token when we're done (kinda like a null)
+    tokens.append({"tag": None, "line": line, "column": column})
+    return tokens
+
+def test_digits():
+    print("test tokenizer")
+    t = tokenize("123")
+    assert t[0]["tag"] == "number"
+    assert t[0]["value"] == 123
+    assert t[1]["tag"] is None
+
+def test_operators():
+    print("test tokenize operators")
+    t = tokenize("+ - * /")
+    tags = [tok["tag"] for tok in t]
+    assert tags == ["+", "-", "*", "/", None]
+
+def test_expressions():
+    print("test tokenize expressions")
+    t = tokenize("1+222*3")
+    assert t[0]["tag"] == "number" and t[0]["value"] == 1
+    assert t[1]["tag"] == "+"
+    assert t[2]["tag"] == "number" and t[2]["value"] == 222
+    assert t[3]["tag"] == "*"
+    assert t[4]["tag"] == "number" and t[4]["value"] == 3
+    assert t[5]["tag"] is None
+
+def test_whitespace():
+    print("test tokenize whitespace")
+    t = tokenize("1 +\t2  \n*    3")
+    assert t[0]["tag"] == "number" and t[0]["value"] == 1
+    assert t[1]["tag"] == "+"
+    assert t[2]["tag"] == "number" and t[2]["value"] == 2
+    assert t[3]["tag"] == "*"
+    assert t[4]["tag"] == "number" and t[4]["value"] == 3
+    assert t[5]["tag"] is None
+
+def test_error():
+    print("test tokenize error")
+    try:
+        t = tokenize("1@@@ +\t2  \n*    3")
+    except Exception as e:
+        assert str(e) == "Unexpected character: '@'"
+        return
+    assert Exception("Error did not happen.")
+     
+
+if __name__ == "__main__":
+    test_digits()
+    test_operators()
+    test_expressions()
+    test_whitespace()
+    test_error()
+    print("done.")
